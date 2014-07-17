02c0676c3d4417e57edd0895ba35f49130556bef
Roman Kazarin <rkazarin@Romans-MacBook-Air.local>
2014-07-17 14:51:36 -0700

    Context Listener stuff

---
+++ src/main/java/com/sample/jersey/app/MyServlet.java
@@ -0,0 +1,22 @@
+package com.sample.jersey.app;
+
+import com.stormpath.sdk.client.Client;
+import javax.servlet.*;
+
+public class MyServlet implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent e) {
+        System.out.println("Context Initialized");
+        System.out.println("Creating new client...");
+
+        StormpathClient stormpathClient = new StormpathClient();
+        Client myClient = stormpathClient.getClient();
+        StormpathUtils.myClient = myClient;
+    }
+
+    public void contextDestroyed(ServletContextEvent e) {
+        System.out.println("Context Destroyed");
+
+    }
+
+}
---
+++ src/main/java/com/sample/jersey/app/StormpathUtils.java
@@ -0,0 +1,9 @@
+package com.sample.jersey.app;
+
+import com.stormpath.sdk.client.Client;
+
+public class StormpathUtils {
+
+    static Client myClient;
+    static String applicationHref = "https://api.stormpath.com/v1/applications/5HRNSljrcYQax0oCQQovT9";
+}
